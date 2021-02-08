d1f42694178629edfef3632875d7ce23c993dca0
afes.okta <afes.okta@gmail.com>
2021-02-08 16:48:41 +0700

    - add qrcode image - add implement absensi controller

--- app/Http/Controllers/AbsensiController.php
+++ app/Http/Controllers/AbsensiController.php
@@ -3,8 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class AbsensiController extends Controller
 {
     //
+    public function __construct() {
+        $this->middleware('auth');
+    }
+
+    public function absensi(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            "email" =>  "required|email",
+            'posyandu'  => "required",
+        ]);
+        if ($request->e =="" && $request->p=="") {
+            return response()->json(["validation_errors" => $validator->errors()]);
+        }
+
+
+    }
 }
